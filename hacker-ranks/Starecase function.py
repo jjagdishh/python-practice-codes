188adcab0f49da143ec1c253ba3b9d43fb7d77ea
jagdish mathpal <28943741+jjagdishh@users.noreply.github.com>
2019-12-27 16:25:33 +0530

    Add files via upload

---
+++ hacker-ranks/Starecase function.py
@@ -0,0 +1,26 @@
+n = 6
+def starecase(n):
+    for i in range(1, n+1):
+        print(("#"*i).rjust(n))
+starecase(n)
+
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    for i in range(1,n+1):
+        print(("#"*i).rjust(n))
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
+"""
